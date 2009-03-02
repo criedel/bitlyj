a8b85a77ca0329eabab18823cb4638e0dea63e9d
chris@rosaloves.com <chris@rosaloves.com@a5c2a118-04db-11de-994d-8308d8631604>
2009-03-02 02:43:45 +0000

    factory

---
+++ src/main/java/com/rosaloves/net/shorturl/bitly/BitlyFactory.java
@@ -0,0 +1,30 @@
+package com.rosaloves.net.shorturl.bitly;
+
+import com.rosaloves.net.shorturl.bitly.auth.Authentication;
+import com.rosaloves.net.shorturl.bitly.auth.InlineAuthentication;
+
+/**
+ * BitlyFactory
+ *
+ * $Id$
+ *
+ * @author Chris Lewis (Mar 1, 2009)
+ */
+public class BitlyFactory {
+	
+	/**
+	 * 
+	 * @param userName The bit.ly login name
+	 * @param apiKey The bit.ly API key
+	 * @return The Bitly implementation configured with an
+	 */
+	public static Bitly newInstance(String userName, String apiKey) {
+		Authentication auth = new InlineAuthentication(userName, apiKey);
+		return new BitlyImpl(auth);
+	}
+	
+	public static Bitly newInstance(Authentication auth) {
+		return new BitlyImpl(auth);
+	}
+	
+}
